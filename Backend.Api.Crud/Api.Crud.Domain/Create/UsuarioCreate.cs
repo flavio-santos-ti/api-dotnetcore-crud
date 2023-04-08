a259c9485c8e35c347d2b2ca18ad2fc1556cb50b
Flávio dos Santos <flavio.santos.ti@outlook.com>
2023-04-08 15:20:41 -0300

    Criação da model Usuário Create.

---
+++ Backend.Api.Crud/Api.Crud.Domain/Create/UsuarioCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Crud.Domain.Create;
+
+public class UsuarioCreate
+{
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public DateTime DataNascto { get; set; }
+    public string Login { get; set; }
+    public string Senha { get; set; }
+}
