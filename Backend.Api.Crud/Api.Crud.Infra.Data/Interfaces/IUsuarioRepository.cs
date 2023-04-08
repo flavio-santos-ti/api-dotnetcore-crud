f81ea7e741e5ff2cd36fe6e17cd107d6481c35f8
Flávio dos Santos <flavio.santos.ti@outlook.com>
2023-04-08 15:03:51 -0300

    Implementação do Repository Design Pattern.

--- Backend.Api.Crud/Api.Crud.Infra.Data/Interfaces/IUsuarioRepository.cs
+++ Backend.Api.Crud/Api.Crud.Infra.Data/Interfaces/IUsuarioRepository.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Api.Crud.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,4 +10,8 @@ namespace Api.Crud.Infra.Data.Interfaces;
 
 public interface IUsuarioRepository
 {
+    Task<Usuario> AAddAsync(Usuario newUsuario);
+    Task<Usuario> UpdateAsync(Usuario usuario);
+    Task DeleteAsync(Usuario usuario);
+    Task<Usuario> GetAsync(Expression<Func<Usuario, bool>> condicao);
 }
