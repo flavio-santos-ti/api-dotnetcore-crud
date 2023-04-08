98d6e7c10fbde8ff0f6776cad42e40767374070a
Flávio dos Santos <flavio.santos.ti@outlook.com>
2023-04-08 14:59:31 -0300

    Refatoração do Usuário Repositório.

--- Backend.Api.Crud/Api.Crud.Infra.Data/Repositories/UsuarioRepository.cs
+++ Backend.Api.Crud/Api.Crud.Infra.Data/Repositories/UsuarioRepository.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,13 +14,13 @@ namespace Api.Crud.Infra.Data.Repositories;
 
 public class UsuarioRepository : RepositoryBase 
 {
-    public UsuarioRepository(DatabaseContext context) : base(context)
-    {
-    }
+    public UsuarioRepository(DatabaseContext context) : base(context) {}
 
-    public async Task<Usuario> AAddAsync(Usuario newUsuario)
-    {
-        return await base.AddAsync<Usuario>(newUsuario);
-    }
+    public async Task<Usuario> AAddAsync(Usuario newUsuario) => await base.AddAsync<Usuario>(newUsuario);
 
+    public async Task<Usuario> UpdateAsync(Usuario usuario) => await base.UpdateAsync<Usuario>(usuario);
+
+    public async Task DeleteAsync(Usuario usuario) => await base.DeleteAsync<Usuario>(usuario);
+
+    public async Task<Usuario> GetAsync(Expression<Func<Usuario, bool>> condicao) => await base.GetAsync<Usuario>(condicao);
 }
